dad6b704cd061364ebbfc7c4a6915514a43346a6
Pros12345 <prosenjitmaigram@gmail.com>
2026-01-05 00:49:32 +0530

    changes for prod

--- src/main/java/eCommerse/config/WebConfig.java
+++ src/main/java/eCommerse/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/api/**").allowedOrigins("http://localhost:4200")
+				registry.addMapping("/api/**").allowedOrigins("ecommerce-client-production.up.railway.app")
 						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS").allowedHeaders("*");
 			}
 		};
